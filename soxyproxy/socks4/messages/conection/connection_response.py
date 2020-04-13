0d107b0a117f851615e0f67eb30dac072c447a2f
shpaker <shpaker@gmail.com>
2020-04-13 22:35:54 +0300

    remove constant in socks4 response

--- soxyproxy/socks4/messages/conection/connection_response.py
+++ soxyproxy/socks4/messages/conection/connection_response.py
@@ -4,6 +4,8 @@ from ipaddress import IPv4Address
 from soxyproxy.socks import ProtocolResponse
 from soxyproxy.socks4.codes import ReplyCodes, ADDRESS_PORT_BYTES_LENGTH, ADDRESS_PORT_BYTES_ORDER
 
+REPLY_VERSION = 0
+
 
 @dataclass
 class ConnectionResponse(ProtocolResponse):
@@ -12,11 +14,8 @@ class ConnectionResponse(ProtocolResponse):
     port: int
     address: IPv4Address
 
-    reply_version: int = 0
-
     @property
     def as_bytes(self):
-
         port_bytes = int.to_bytes(self.port, ADDRESS_PORT_BYTES_LENGTH, ADDRESS_PORT_BYTES_ORDER)
 
-        return bytes([self.reply_version, self.reply.value]) + port_bytes + self.address.packed
+        return bytes([REPLY_VERSION, self.reply.value]) + port_bytes + self.address.packed
