11444543861d2c41c71be43e19e3a914c3422ace
shpaker <shpaker@gmail.com>
2020-04-15 07:39:13 +0300

    env vars parsing with environs

--- soxyproxy/app.py
+++ soxyproxy/app.py
@@ -1,13 +1,16 @@
 import asyncio
 import logging
-from typing import Optional, Coroutine
+from typing import Optional
 
+from environs import Env
 from typer import Option, Typer, echo, BadParameter
 
-from soxyproxy import Socks, Socks4, Socks5
+from soxyproxy import Socks4, Socks5
+from soxyproxy.protocols import Protocols
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)-8s %(message)s', level=logging.DEBUG)
 
 app = Typer()
-logging.basicConfig(format='[%(asctime)s] %(levelname)-8s %(message)s', level=logging.DEBUG)
 
 DEFAULT_HOST = '0.0.0.0'
 DEFAULT_PORT = 1080
@@ -15,8 +18,8 @@ DEFAULT_PORT = 1080
 
 @app.command()
 def socks4(host: str = Option(DEFAULT_HOST), port: int = Option(DEFAULT_PORT)) -> None:
-    server = Socks4()
-    start_server(server=server, host=host, port=port)
+    proxy = Socks4()
+    asyncio.run(proxy.run(host=host, port=port))
 
 
 @app.command()
@@ -24,25 +27,37 @@ def socks5(host: str = Option(DEFAULT_HOST),
            port: int = Option(DEFAULT_PORT),
            username: Optional[str] = Option(None),
            password: Optional[str] = Option(None)) -> None:
-
-    if username and not password or not username and password:
+    try:
+        proxy = Socks5(username=username, password=password)
+    except:
         raise BadParameter('please, specify username and password')
 
-    server = Socks5(username=username, password=password)
-    start_server(server=server, host=host, port=port)
+    asyncio.run(proxy.run(host=host, port=port))
 
 
-def start_server(server: Socks, host: str, port: int) -> None:
+def main():
+    env = Env()
+    env_proxy_protocol: str = env.str('PROXY_PROTOCOL', None)
 
-    server_coroutine: Coroutine[str, int] = server.run(host=host, port=port)
+    if env_proxy_protocol is None:
+        app()
+        return
 
-    echo(f'Start SOCKS{server.version.value} server at {host}:{port}')
+    echo('Configuration with environment variables')
 
-    try:
-        asyncio.run(server_coroutine)
-    except KeyboardInterrupt:
-        echo('Server stopped')
+    protocol = Protocols[env_proxy_protocol.upper()]
+
+    host = env.str('PROXY_HOST', DEFAULT_HOST)
+    port = env.int('PROXY_PORT', DEFAULT_PORT)
+    user = env.str('PROXY_USER', None)
+    password = env.str('PROXY_PASSWORD', None)
+
+    proxy = Socks4() if protocol is Protocols.SOCKS4 else Socks5(user, password)
+    asyncio.run(proxy.run(host, port))
 
 
 if __name__ == "__main__":
-    app()
+    try:
+        main()
+    except KeyboardInterrupt:
+        echo('Server stopped')
