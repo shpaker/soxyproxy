9d7332e2180bfd4cd590444affbc1f48bbf81183
A.Shpak <aleksander.shpak.als@gmail.com>
2024-11-29 23:09:13 +0300

    -

--- soxyproxy/__init__.py
+++ soxyproxy/__init__.py
@@ -5,6 +5,7 @@ from soxyproxy._errors import (
     RejectError,
     ResolveDomainError,
 )
+from soxyproxy._ruleset import Rule, Ruleset
 from soxyproxy._service import ProxyService
 from soxyproxy._socks4 import Socks4
 from soxyproxy._socks5 import Socks5
@@ -14,8 +15,6 @@ from soxyproxy._types import (
     Destination,
     ProxySocks,
     ProxyTransport,
-    Rule,
-    RuleSet,
 )
 
 __all__ = [
@@ -32,7 +31,7 @@ __all__ = [
     'RejectError',
     'ResolveDomainError',
     'Rule',
-    'RuleSet',
+    'Ruleset',
     # protocol implementations
     'Socks4',
     'Socks5',
